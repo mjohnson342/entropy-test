c884f248bd9fda5b71ac82b5e8ab907bb841e3d2
mjohnson342 <mitchelljohnson114@gmail.com>
2018-06-07 09:51:13 -0700

    Update ent-test.py

--- ent-test.py
+++ ent-test.py
@@ -3,5 +3,4 @@ def pow(a,b):
   pw2 = "s9d2k2nfi2dmdb29fhqkx9381jf8skch2kdn43r"
   pw3 = "2cf24dba5fb0a30e26e83b2ac5b9e29e"
   pw_short = "jg2pbe2bv2lgzqrm"
-  a = "Lorem ipsum dolor sit amet, mei augue facilisi cotidieque no 0owba20htao78625wrzm8 sensibus suavitate"
-  key1 = "3vpwzjj5vknwayr9p80dtds3z8pio0"
+  a = "6794h98tk4s302jli0858pmvpdope1daetk1siljt3cjvmaks8"
